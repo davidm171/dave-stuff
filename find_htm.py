7fcea980edb2ae055e751a23be1b2b071bd78f25
David McCloy <davidm171@gmail.com>
2018-10-13 16:59:13 +0100

    Added cross referecnes file to repo

--- find_htm.py
+++ find_htm.py
@@ -4,7 +4,7 @@ import os
 import fnmatch
 import sys
 import re
-from htm_objects import HtmInfo
+from htm_object import HtmInfo
 
 def open_file(file):
     with open(file, 'r') as f:
@@ -36,60 +36,35 @@ def h1_print(file_texts):
             
 top = r"C:\test_project"
 filepat = "*.htm"
-            
+
+# Defines the generator ------------------------------------------------------------------------------
+
 gen_find = (os.path.join(path,name) for path, dirlist, filelist in os.walk(top)
                                     for name in fnmatch.filter(filelist,filepat))
 file_text_strings = open_file_y(gen_find)
 h1s = h1_print(file_text_strings)
+
+# Runs the generator ---------------------------------------------------------------------------------
 
 for h in h1s:
     filename = path_transfer.split("\\")[-1]
     htm_file_object = HtmInfo(filename, path_transfer, h[1], h[2], h[3])
     htm_file_object.add()
     italics_list = h[2]
-    if italics_list:
-        htm_file_object.add_italics()
+    
+    htm_file_object.add_italics() if italics_list
         
     prospective_sys_param_list = h[3]
     if prospective_sys_param_list:
         htm_file_object.add_params()
 
+
+
+
     
-
-print "#############################################################################\n\n"
-
-file_count = 0
-italics_file_count = 0
-prosp_sys_param_file_count = 0
-
-
-
-for entry in HtmInfo.htm_object_list:
-    print "Path: ", entry.htm_path
-    # print "Name: ", entry.htm_name
-    # print "Heading: ", entry.htm_h1
-    # print "\n\n"
-    file_count +=1
-    
-for entry in HtmInfo.htm_italics_list:
-    print "Path: ", entry.htm_path
-    # print "Name: ", entry.htm_name
-    # print "Heading: ", entry.htm_h1
-    # print "Italics: ", entry.italics_list
-    # print "\n\n"
-    italics_file_count += 1
-
-for entry in HtmInfo.htm_params_list:
-    # print "Path: ", entry.htm_path
-    # print "Name: ", entry.htm_name
-    # print "Heading: ", entry.htm_h1
-    # print "Params: ", entry.prospective_sys_param_list
-    # print "\n\n"
-    prosp_sys_param_file_count += 1
-    
-print "Total number of files           : ", file_count, len(HtmInfo.htm_object_list)
-print "Files with italics              : ", italics_file_count, len(HtmInfo.htm_italics_list)
-print "Files with system params (maybe): ", prosp_sys_param_file_count, len(HtmInfo.htm_params_list)
+print "Total number of files           : ", len(HtmInfo.htm_object_list)
+print "Files with italics              : ", len(HtmInfo.htm_italics_list)
+print "Files with system params (maybe): ", len(HtmInfo.htm_params_list)
 
 itals = [x.htm_path for x in HtmInfo.htm_italics_list]
 params = [p.htm_path for p in HtmInfo.htm_params_list]
